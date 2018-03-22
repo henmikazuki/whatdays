029ead0c1897531c62da35a3816a9c7b8f72351b
辺見和輝 <minskeh@gmail.com>
2018-03-23 01:39:30 +0900

    内容を一部更新

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
+/*
 let url = 'https://ja.wikipedia.org/w/api.php?';
 url += 'format=json&utf8&action=query&prop=revisions&rvprop=content&titles=1月1日';
+*/
 $(function() {
   $('#btn').on('click', function() {
+    let url = 'https://ja.wikipedia.org/w/api.php?';
+    url += 'format=json&utf8&action=query&prop=revisions&rvprop=content&titles=1月1日';
     $.ajax({
       url: url,
       dataType: 'jsonp',
     }).done(function(data) {
       if (data.query) {
-        console.log(data);
+        setData(data);
+//        console.log(data);
       } else {
         alert('データが見つかりませんでした');
       }
@@ -16,3 +21,8 @@ $(function() {
     });
   });
 });
+
+function setData(data) {
+  let result = data;
+  $('#show').html(result);
+}
